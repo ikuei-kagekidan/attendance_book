d77a6e84540f34e75f5ac17f97be64e0f58905b2
izumi-reon <s17505@salesio-sp.ac.jp>
2021-09-23 22:20:22 +0000

    made database structure

--- attendance_book/admin.py
+++ attendance_book/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Person, Subject, Student, Teacher, Timetable, Attendance
+
+admin.site.register(Person)
+admin.site.register(Subject)
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(Timetable)
+admin.site.register(Attendance)
--- attendance_book/models.py
+++ attendance_book/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Student(models.Model):
+    student_num = models.CharField(max_length=6, primary_key=True)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    class_num = models.IntegerField()
+    
+
+class Teacher(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+
+class Timetable(models.Model):
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    day_of_week = models.CharField(max_length=50)
+    start_period = models.IntegerField()
+    end_period = models.IntegerField()
+
+
+class Attendance(models.Model):
+    date = models.DateTimeField()
+    period = models.IntegerField()
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    status = models.CharField(max_length=50)
