3b3b9b2c288ede77ab186bf4226db2ce0c12eb00
izumi-reon <s17505@salesio-sp.ac.jp>
2021-09-24 03:16:34 +0000

    made example data

--- attendance_book/models.py
+++ attendance_book/models.py
@@ -6,21 +6,28 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
 
 
 class Subject(models.Model):
     name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
 
 
 class Student(models.Model):
     student_num = models.CharField(max_length=6, primary_key=True)
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
     class_num = models.IntegerField()
-    
+    def __str__(self):
+        return str(self.class_num) + " " + self.person.name
 
 class Teacher(models.Model):
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.subject.name + " " + self.person.name
 
 
 class Timetable(models.Model):
@@ -28,7 +35,8 @@ class Timetable(models.Model):
     day_of_week = models.CharField(max_length=50)
     start_period = models.IntegerField()
     end_period = models.IntegerField()
-
+    def __str__(self):
+        return self.day_of_week + " " + self.subject.name
 
 class Attendance(models.Model):
     date = models.DateTimeField()
