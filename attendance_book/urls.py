be3617a7d2dec356dcadcffa83811b7640ab28a2
s17526 <86092682+s17526@users.noreply.github.com>
2021-10-29 14:12:10 +0900

    Update urls.py

--- attendance_book/urls.py
+++ attendance_book/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('teacher/input/', views.teach_in, name='teacher/input/'),
-    path('teacher/aggregation/', views.teach_agg, name='teacher/aggregation/'),
-    path('student/', views.student, name='student/'),
+    path('teacher/input/', views.teach_in, name='teacher-input'),
+    path('teacher/aggregation/', views.teach_agg, name='teacher-aggregation'),
+    path('student/', views.student, name='student'),
 ]
