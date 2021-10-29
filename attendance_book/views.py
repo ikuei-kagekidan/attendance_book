de90c4abe0e3fad2f536205ff6603a179cae37d0
s17526 <s17526@salesio-sp.ac.jp>
2021-10-29 02:17:21 +0000

    kadai

--- attendance_book/views.py
+++ attendance_book/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'attendance_book/index.html')
+
+def teach_in(request):
+    return render(request, 'attendance_book/teach_in.html')
+
+def teach_agg(request):
+    return render(request, 'attendance_book/teach_agg.html')
+
+def student(request):
+    return render(request, 'attendance_book/student.html')
--- mysite/urls.py
+++ mysite/urls.py
@@ -3,5 +3,9 @@ from django.urls import include, path
 
 urlpatterns = [
     path('attendance_book/', include('attendance_book.urls')),
+    path('attendance_book/teacher/input/', include('attendance_book.urls')),
+    path('attendance_book/teacher/aggregation/', include('attendance_book.urls')),
+    path('attendance_book/student/', include('attendance_book.urls')),
     path('admin/', admin.site.urls),
 ]
+
