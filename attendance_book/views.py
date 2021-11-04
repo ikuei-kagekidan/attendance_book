223c49b40e4ec77d704b3eb6809efde2a78416fc
izumi-reon <s17505@salesio-sp.ac.jp>
2021-11-04 20:47:47 +0000

    仮の表示画面

--- attendance_book/views.py
+++ attendance_book/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 
+from .models import Attendance, Student
+
+import datetime
 
 def index(request):
     return render(request, 'attendance_book/index.html')
 
 def teach_in(request):
-    return render(request, 'attendance_book/teach_in.html')
+    today = datetime.date.today()
+    a = Attendance.objects.filter(date=today)
+    s = Student.objects.all()
+    if not a:
+        return render(request, 'attendance_book/teach_in.html')
+    context = {
+        "student_list": s,
+        "today_attendance_list": a
+    }
+    return render(request, 'attendance_book/teach_in.html', context)
 
 def teach_agg(request):
     return render(request, 'attendance_book/teach_agg.html')
