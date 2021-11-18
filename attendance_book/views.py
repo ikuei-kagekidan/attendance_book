ee9643e074e78baacfd4910e51d3292f6caa11fb
izumi-reon <s17505@salesio-sp.ac.jp>
2021-11-18 13:17:17 +0000

    tmp

--- attendance_book/views.py
+++ attendance_book/views.py
@@ -1,23 +1,36 @@
 from django.shortcuts import render
+from django.utils import timezone
 
-from .models import Attendance, Student
-
-import datetime
+from .models import Attendance, Student, Timetable
 
 def index(request):
     return render(request, 'attendance_book/index.html')
 
 def teach_in(request):
-    today = datetime.date.today()
+    today = timezone.now().date()
     a = Attendance.objects.filter(date=today)
-    s = Student.objects.all()
-    if not a:
-        return render(request, 'attendance_book/teach_in.html')
+    s = Student.objects.all().order_by("class_num")
+    
+    subject_list = []
+    
     context = {
+        "t": today,
         "student_list": s,
-        "today_attendance_list": a
+        "subject_list": subject_list
     }
+    
+    #if not subject_list:
+    #    today_timetable = Timetable.objects.filter(day_of_week=today.weekday())
+    
     return render(request, 'attendance_book/teach_in.html', context)
+    
+    #if not a:
+    #    return render(request, 'attendance_book/teach_in.html')
+    #context = {
+    #    "student_list": s,
+    #    "today_attendance_list": a
+    #}
+    #return render(request, 'attendance_book/teach_in.html', context)
 
 def teach_agg(request):
     return render(request, 'attendance_book/teach_agg.html')
