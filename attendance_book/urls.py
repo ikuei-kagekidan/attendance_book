d4fdc16682db3714b4f7d0dcbc8bd036e315db37
izumi-reon <s17505@salesio-sp.ac.jp>
2021-11-20 08:55:33 +0000

    tmp5

--- attendance_book/urls.py
+++ attendance_book/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('teacher/input/', views.teach_in, name='teacher-input'),
     path('teacher/aggregation/', views.teach_agg, name='teacher-aggregation'),
     path('student/', views.student, name='student'),
-    path('teacher/input/create-attendance/', views.create_attendance, name='create-attendance')
 ]
--- attendance_book/views.py
+++ attendance_book/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.http import HttpResponse
 
 from .models import Attendance, Student, Timetable
 
@@ -9,6 +10,22 @@ def index(request):
     return render(request, 'attendance_book/index.html')
 
 def teach_in(request):
+    if request.method == 'POST':
+        date_string = request.POST["date"]
+        date_string = "2021-11-19"
+        date = datetime.datetime.strptime(date_string, "%Y-%m-%d").date()
+        week = date.weekday()
+        timetable = Timetable.objects.filter(day_of_week=week)
+        student = Student.objects.all().order_by("class_num")
+        print(timetable)
+        for tt in timetable:
+            sp = tt.start_period
+            pl = tt.period_length
+            for s in student:
+                for i in range(pl):
+                    at = Attendance.objects.create(date=date, period=sp+i, student=s, teacher=tt.teacher, subject=tt.subject, status=0)
+                
+    
     today = timezone.now().date()
     a = Attendance.objects.filter(date=today)
     s = Student.objects.all().order_by("class_num")
@@ -34,18 +51,18 @@ def teach_in(request):
     #}
     #return render(request, 'attendance_book/teach_in.html', context)
 
-def create_attendance(request):
-    date_string = request.POST["date"]
-    date = datetime.datetime.strptime(date_string, "%Y-%m-%d").date()
-    week = date.week()
-    timetable = Timetable.objects.filter(day_of_week=week)
-    student = Student.objects.all().order_by("class_num")
-    for tt in timetable:
-        sp = tt.start_period
-        pl = tt.period_length
-        for s in student:
-            for i in range(pl):
-                at = Attendance.objects.creat(date=date, period=sp+i, student=s, teacher=tt.teacher, subject=tt.subject, status=0)
+#def create_attendance(request):
+#    date_string = request.POST["date"]
+#    date = datetime.datetime.strptime(date_string, "%Y-%m-%d").date()
+#    week = date.week()
+#    timetable = Timetable.objects.filter(day_of_week=week)
+#    student = Student.objects.all().order_by("class_num")
+#    for tt in timetable:
+#        sp = tt.start_period
+#        pl = tt.period_length
+#        for s in student:
+#            for i in range(pl):
+#                at = Attendance.objects.creat(date=date, period=sp+i, student=s, teacher=tt.teacher, subject=tt.subject, status=0)
             
 
 def teach_agg(request):
