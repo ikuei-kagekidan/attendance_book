785872a6b26bd3ad162faa8ddd06fef7a26db1a1
izumi-reon <s17505@salesio-sp.ac.jp>
2021-12-15 16:39:10 +0000

    made agg-csv-output

--- attendance_book/urls.py
+++ attendance_book/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('teacher/input/', views.teach_in, name='teacher-input'),
     path('teacher/aggregation/', views.teach_agg, name='teacher-aggregation'),
     path('student/', views.student, name='student'),
-    path('teacher/input/post', views.teach_in_post, name='teacher-input-post'),
-    path('teacher/aggregation/post', views.teach_agg_post, name='teacher-aggregation-post'),
+    path('teacher/input/post/', views.teach_in_post, name='teacher-input-post'),
+    path('teacher/aggregation/post/', views.teach_agg_post, name='teacher-aggregation-post'),
+    path('teacher/aggregation/download/', views.teach_agg_download, name='teacher-aggregation-download'),
 ]
